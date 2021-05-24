9c419151aa8f8532b04342fa17e8fe689dac2216
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-05-24 14:28:06 +0700

    :boom:

--- plugins/install-formatter.sh
+++ plugins/install-formatter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-composer install --working-dir=~/.config/nvim/plugins/formatter/php-cs-fixer
-npm install --prefix ~/.config/nvim/plugins/formatter/prettier
-npm install --prefix ~/.config/nvim/plugins/formatter/lua-ftm
+composer install --working-dir=$HOME/.config/nvim/plugins/formatter/php-cs-fixer
+npm install --prefix $HOME/.config/nvim/plugins/formatter/prettier
+npm install --prefix $HOME/.config/nvim/plugins/formatter/lua-ftm
 curl -sS https://webinstall.dev/shfmt | bash
