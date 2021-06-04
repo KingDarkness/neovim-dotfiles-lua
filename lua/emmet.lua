22e93fc29990ec904c06b74d0b1cb0716f3d2b43
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-06-04 14:54:43 +0700

    :boom:

--- lua/emmet.lua
+++ lua/emmet.lua
@@ -1,3 +1,8 @@
 local g = vim.g -- a table to access global variables
 g.user_emmet_leader_key = ","
 g.user_emmet_mode = "i"
+g.user_emmet_install_global = 0
+
+vim.api.nvim_exec([[
+  autocmd FileType html,css EmmetInstall
+]], false)
