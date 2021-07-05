7701f93efdcab16a2a7b5f59449186f4f5c40b99
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-07-05 10:19:54 +0700

    :boom:

--- lua/tree.lua
+++ lua/tree.lua
@@ -15,6 +15,8 @@ g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ":t"
 g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
+g.nvim_tree_lsp_diagnostics = 1
+g.nvim_tree_update_cwd = 1
 
 g.nvim_tree_show_icons = {
     git = 1,
@@ -36,12 +38,14 @@ g.nvim_tree_icons = {
         default = "",
         open = "",
         symlink = ""
+    },
+    lsp = {
+        hint = "",
+        info = "",
+        warning = "",
+        error = ""
     }
 }
-
-local get_lua_cb = function(cb_name)
-    return string.format(":lua require'nvim-tree'.on_keypress('%s')<CR>", cb_name)
-end
 
 -- Mappings for nvimtree
 
@@ -55,30 +59,37 @@ vim.api.nvim_set_keymap(
     }
 )
 
+local tree_cb = require "nvim-tree.config".nvim_tree_callback
 g.nvim_tree_bindings = {
-    ["<CR>"] = get_lua_cb("edit"),
-    ["o"] = get_lua_cb("edit"),
-    ["<2-LeftMouse>"] = get_lua_cb("edit"),
-    ["<2-RightMouse>"] = get_lua_cb("cd"),
-    ["<C-]>"] = get_lua_cb("cd"),
-    ["<C-v>"] = get_lua_cb("vsplit"),
-    ["<C-h>"] = get_lua_cb("split"),
-    ["<C-t>"] = get_lua_cb("tabnew"),
-    ["<BS>"] = get_lua_cb("close_node"),
-    ["<S-CR>"] = get_lua_cb("close_node"),
-    ["<Tab>"] = get_lua_cb("preview"),
-    ["I"] = get_lua_cb("toggle_ignored"),
-    ["H"] = get_lua_cb("toggle_dotfiles"),
-    ["R"] = get_lua_cb("refresh"),
-    ["a"] = get_lua_cb("create"),
-    ["d"] = get_lua_cb("remove"),
-    ["r"] = get_lua_cb("rename"),
-    ["<C-r>"] = get_lua_cb("full_rename"),
-    ["x"] = get_lua_cb("cut"),
-    ["c"] = get_lua_cb("copy"),
-    ["p"] = get_lua_cb("paste"),
-    ["[c"] = get_lua_cb("prev_git_item"),
-    ["]c"] = get_lua_cb("next_git_item"),
-    ["-"] = get_lua_cb("dir_up"),
-    ["q"] = get_lua_cb("close")
+    {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
+    {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
+    {key = "<C-v>", cb = tree_cb("vsplit")},
+    {key = "<C-x>", cb = tree_cb("split")},
+    {key = "<C-t>", cb = tree_cb("tabnew")},
+    {key = "<", cb = tree_cb("prev_sibling")},
+    {key = ">", cb = tree_cb("next_sibling")},
+    {key = "P", cb = tree_cb("parent_node")},
+    {key = "<BS>", cb = tree_cb("close_node")},
+    {key = "<S-CR>", cb = tree_cb("close_node")},
+    {key = "<Tab>", cb = tree_cb("preview")},
+    {key = "K", cb = tree_cb("first_sibling")},
+    {key = "J", cb = tree_cb("last_sibling")},
+    {key = "I", cb = tree_cb("toggle_ignored")},
+    {key = "H", cb = tree_cb("toggle_dotfiles")},
+    {key = "R", cb = tree_cb("refresh")},
+    {key = "a", cb = tree_cb("create")},
+    {key = "d", cb = tree_cb("remove")},
+    {key = "r", cb = tree_cb("rename")},
+    {key = "<C-r>", cb = tree_cb("full_rename")},
+    {key = "x", cb = tree_cb("cut")},
+    {key = "c", cb = tree_cb("copy")},
+    {key = "p", cb = tree_cb("paste")},
+    {key = "y", cb = tree_cb("copy_name")},
+    {key = "Y", cb = tree_cb("copy_path")},
+    {key = "gy", cb = tree_cb("copy_absolute_path")},
+    {key = "[c", cb = tree_cb("prev_git_item")},
+    {key = "]c", cb = tree_cb("next_git_item")},
+    {key = "-", cb = tree_cb("dir_up")},
+    {key = "q", cb = tree_cb("close")},
+    {key = "?", cb = tree_cb("toggle_help")}
 }
