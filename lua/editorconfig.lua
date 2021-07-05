bf3fdacbfa073589ad02f7ee08e9b73f4a758adf
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-07-05 16:10:38 +0700

    :boom:

--- lua/editorconfig.lua
+++ lua/editorconfig.lua
@@ -1,6 +1,3 @@
 local g = vim.g -- a table to access global variables
-
-local handle = io.popen("which editorconfig")
-local result = handle:read("*a")
-g.EditorConfig_exec_path = result
+g.EditorConfig_exec_path = "editorconfig"
 g.EditorConfig_core_mode = "external_command"
