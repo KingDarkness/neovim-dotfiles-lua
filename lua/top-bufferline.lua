0cbbf99c8ce2cac80c10d55023762fa6d4e22293
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-09-30 10:24:00 +0700

    :boom:

--- lua/top-bufferline.lua
+++ lua/top-bufferline.lua
@@ -20,34 +20,7 @@ require "bufferline".setup {
         view = "multiwindow",
         show_buffer_close_icons = false,
         separator_style = "thin",
-        mappings = "true",
         sort_by = "directory"
-        -- custom_areas = {
-        --   right = function()
-        --     local result = {}
-        --     local error = vim.lsp.diagnostic.get_count(0, [[Error]])
-        --     local warning = vim.lsp.diagnostic.get_count(0, [[Warning]])
-        --     local info = vim.lsp.diagnostic.get_count(0, [[Information]])
-        --     local hint = vim.lsp.diagnostic.get_count(0, [[Hint]])
-
-        --     if error ~= 0 then
-        --     result[1] = {text = "  " .. error, guifg = "#EC5241"}
-        --     end
-
-        --     if warning ~= 0 then
-        --     result[2] = {text = "  " .. warning, guifg = "#EFB839"}
-        --     end
-
-        --     if hint ~= 0 then
-        --     result[3] = {text = "  " .. hint, guifg = "#A3BA5E"}
-        --     end
-
-        --     if info ~= 0 then
-        --     result[4] = {text = "  " .. info, guifg = "#7EA9A7"}
-        --   end
-        --   return result
-        -- end
-        -- }
     },
     -- bar colors!!
     highlights = {
