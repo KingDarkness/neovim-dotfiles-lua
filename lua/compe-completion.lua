1e82ce9e15b40e50caa3e26bdd248937510706e5
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-10-05 17:26:27 +0700

    :boom:

--- lua/compe-completion.lua
+++ lua/compe-completion.lua
@@ -1,6 +1,15 @@
 vim.o.completeopt = "menu,menuone,noselect"
 local cmp = require "cmp"
 
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col(".") - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
+end
+
 cmp.setup(
     {
         snippet = {
@@ -16,12 +25,40 @@ cmp.setup(
             end
         },
         mapping = {
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<C-n>"] = cmp.mapping.select_next_item(),
             ["<C-d>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.close(),
-            ["<CR>"] = cmp.mapping.confirm({select = true}),
-            ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "s"})
+            ["<CR>"] = cmp.mapping.confirm(
+                {
+                    behavior = cmp.ConfirmBehavior.Insert,
+                    select = true
+                }
+            ),
+            ["<Tab>"] = cmp.mapping(
+                function(fallback)
+                    if vim.fn.pumvisible() == 1 then
+                        vim.fn.feedkeys(t("<C-n>"), "n")
+                    elseif check_back_space() then
+                        vim.fn.feedkeys(t("<tab>"), "n")
+                    else
+                        fallback()
+                    end
+                end,
+                {"i", "s"}
+            ),
+            ["<S-Tab>"] = cmp.mapping(
+                function(fallback)
+                    if vim.fn.pumvisible() == 1 then
+                        vim.fn.feedkeys(t("<C-p>"), "n")
+                    else
+                        fallback()
+                    end
+                end,
+                {"i", "s"}
+            )
         },
         sources = {
             {name = "nvim_lsp"},
@@ -34,16 +71,24 @@ cmp.setup(
             {name = "treesitter"},
             {name = "path"},
             {name = "buffer"}
-        }
+        },
+        formatting = {
+            format = function(entry, vim_item)
+                -- fancy icons and a name of kind
+                vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+                -- set a name for each source
+                vim_item.menu =
+                    ({
+                    buffer = "[Buffer]",
+                    nvim_lsp = "[LSP]",
+                    vsnip = "[Vsnip]",
+                    nvim_lua = "[Lua]",
+                    path = "[Path]",
+                    treesitter = "[Treesitter]"
+                })[entry.source.name]
+                return vim_item
+            end
+        },
+        completion = {completeopt = "menu,menuone,noinsert"}
     }
 )
-
-local lspkind = require("lspkind")
-cmp.setup {
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.kind = lspkind.presets.default[vim_item.kind]
-            return vim_item
-        end
-    }
-}
