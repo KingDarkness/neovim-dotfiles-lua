83240c19e78c72b440235691dfac8c6de5088e60
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-10-08 11:54:46 +0700

    :boom:

--- lua/telescopes.lua
+++ lua/telescopes.lua
@@ -6,34 +6,16 @@ require("telescope").setup {
             "--hidden",
             "--files"
         },
+        layout_config = {
+            prompt_position = "bottom"
+        },
         prompt_prefix = " ",
         selection_caret = " ",
         entry_prefix = "  ",
-        initial_mode = "insert",
-        selection_strategy = "reset",
-        sorting_strategy = "descending",
-        layout_strategy = "horizontal",
-        layout_config = {
-            prompt_position = "bottom",
-            width = 0.75,
-            height = 0.75,
-            preview_cutoff = 120,
-            horizontal = {
-                mirror = false
-            },
-            vertical = {
-                mirror = false
-            }
-        },
         file_sorter = require "telescope.sorters".get_fuzzy_file,
         file_ignore_patterns = {".git"},
         generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
         path_display = {},
-        winblend = 0,
-        border = {},
-        borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
-        color_devicons = true,
-        use_less = true,
         set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
         file_previewer = require "telescope.previewers".vim_buffer_cat.new,
         grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
@@ -52,6 +34,11 @@ require("telescope").setup {
             override_file_sorter = true, -- override the file sorter
             case_mode = "smart_case" -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
+        }
+    },
+    pickers = {
+        find_files = {
+            theme = "ivy"
         }
     }
 }
