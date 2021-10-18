36c91e9442ab638330326d36da20cddd987324d8
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-10-18 17:39:22 +0700

    :boom:

--- lua/test.lua
+++ lua/test.lua
@@ -11,20 +11,38 @@ vim.api.nvim_exec(
   let test#neovim#term_position = "topleft"
   let test#neovim#term_position = "vert"
   let test#neovim#term_position = "vert botright 100"
+  let g:docker_container_name = ''
+
+    function DoTest(type)
+        if g:docker_container_name == ''
+            let name = input('Enter docker container name: ')
+            let g:docker_container_name = name
+            redraw
+        endif
+
+        if a:type == "TestNearest"
+            :TestNearest
+        elseif a:type == "TestFile"
+            :TestFile
+        elseif a:type == "TestSuite"
+            :TestSuite
+        else
+            :TestLast
+        endif
+    endfunction
 
   function! DockerTransform(cmd) abort
-    let docker_container_name = '3t-wallet'
     let phpunit_xml = '/var/www/html/phpunit.xml'
-    return 'docker exec ' . docker_container_name . ' phpdbg -qrr ' . a:cmd . ' -c ' . phpunit_xml . ' --debug --colors=always'
+    return 'docker exec ' . g:docker_container_name . ' phpdbg -qrr ' . a:cmd . ' -c ' . phpunit_xml . ' --debug --colors=always'
   endfunction
 
   let g:test#custom_transformations = {'docker': function('DockerTransform')}
   let g:test#transformation = 'docker'
 
-  nmap <silent> <leader>tm :TestNearest<cr>
-  nmap <silent> <leader>tf :TestFile<cr>
-  nmap <silent> <leader>ta :TestSuite<cr>
-  nmap <silent> <leader>tp :TestLast<cr>
+  nmap <silent> <leader>tm :call DoTest("TestNearest")<cr>
+  nmap <silent> <leader>tf :call DoTest("TestFile")<cr>
+  nmap <silent> <leader>ta :call DoTest("TestSuite")<cr>
+  nmap <silent> <leader>tp :call DoTest("TestLast")<cr>
 ]],
     false
 )
