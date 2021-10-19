f72d593e967fc20815b3470e16b183a542890ee2
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2021-10-19 09:00:03 +0700

    :boom:

--- lua/tree.lua
+++ lua/tree.lua
@@ -56,7 +56,21 @@ require "nvim-tree".setup {
     -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
     update_cwd = false,
     -- show lsp diagnostics in the signcolumn
-    lsp_diagnostics = false,
+    diagnostics = {
+        enable = false,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = ""
+        }
+    },
+    update_to_buf_dir = {
+        -- enable the feature
+        enable = true,
+        -- allow to open the tree if it was previously closed
+        auto_open = true
+    },
     -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
     update_focused_file = {
         -- enables the feature
