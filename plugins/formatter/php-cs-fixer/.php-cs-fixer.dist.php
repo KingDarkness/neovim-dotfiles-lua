517907f2b036087a26e7629d5a17130282be1e64
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2022-04-14 12:06:49 +0700

    :boom:

--- plugins/formatter/php-cs-fixer/.php-cs-fixer.dist.php
+++ plugins/formatter/php-cs-fixer/.php-cs-fixer.dist.php
@@ -1,11 +1,13 @@
 <?php
 
 $rules = [
-        '@PSR2' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'new_with_braces' => true,
-        'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['const', 'class', 'function']],
-    ];
+    '@PSR2' => true,
+    'array_indentation' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'trim_array_spaces' => true,
+    'new_with_braces' => true,
+    'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['const', 'class', 'function']],
+];
 
 $config = new PhpCsFixer\Config();
 
