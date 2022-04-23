33ceb51feccb1e182e426070944651210be296b1
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2022-04-23 14:44:08 +0700

    :boom:

--- lua/config/cmp-completion.lua
+++ lua/config/cmp-completion.lua
@@ -37,56 +37,60 @@ function M.setup()
                     -- vim.fn["UltiSnips#Anon"](args.body)
                 end
             },
-            mapping = {
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm(
-                    {
-                        select = true,
-                        behavior = cmp.ConfirmBehavior.Insert
-                    }
-                ),
-                ["<Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif vim.fn["vsnip#available"](1) == 1 then
-                            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                        elseif has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-                        end
-                    end,
-                    {"i", "s"}
-                ),
-                ["<S-Tab>"] = cmp.mapping(
-                    function()
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                            feedkey("<Plug>(vsnip-jump-prev)", "")
-                        end
-                    end,
-                    {"i", "s"}
-                )
-            },
-            sources = {
-                {name = "nvim_lsp"},
-                -- For vsnip user.
-                {name = "vsnip"},
-                -- For luasnip user.
-                -- {name = "luasnip"},
-                -- For ultisnips user.
-                -- { name = 'ultisnips' },
-                {name = "treesitter"},
-                {name = "path"},
-                {name = "buffer"}
-            },
+            mapping = cmp.mapping.preset.insert(
+                {
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.close(),
+                    ["<CR>"] = cmp.mapping.confirm(
+                        {
+                            select = true,
+                            behavior = cmp.ConfirmBehavior.Insert
+                        }
+                    ),
+                    ["<Tab>"] = cmp.mapping(
+                        function(fallback)
+                            if cmp.visible() then
+                                cmp.select_next_item()
+                            elseif vim.fn["vsnip#available"](1) == 1 then
+                                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                            elseif has_words_before() then
+                                cmp.complete()
+                            else
+                                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                            end
+                        end,
+                        {"i", "s"}
+                    ),
+                    ["<S-Tab>"] = cmp.mapping(
+                        function()
+                            if cmp.visible() then
+                                cmp.select_prev_item()
+                            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                                feedkey("<Plug>(vsnip-jump-prev)", "")
+                            end
+                        end,
+                        {"i", "s"}
+                    )
+                }
+            ),
+            sources = cmp.config.sources(
+                {
+                    {name = "nvim_lsp"},
+                    -- For vsnip user.
+                    {name = "vsnip"},
+                    -- For luasnip user.
+                    -- {name = "luasnip"},
+                    -- For ultisnips user.
+                    -- { name = 'ultisnips' },
+                    {name = "treesitter"},
+                    {name = "path"},
+                    {name = "buffer"}
+                }
+            ),
             formatting = {
                 format = function(entry, vim_item)
                     -- fancy icons and a name of kind
@@ -110,9 +114,12 @@ function M.setup()
     require "cmp".setup.cmdline(
         ":",
         {
-            sources = {
-                {name = "cmdline"}
-            }
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources(
+                {
+                    {name = "cmdline"}
+                }
+            )
         }
     )
 
@@ -120,6 +127,7 @@ function M.setup()
     cmp.setup.cmdline(
         "/",
         {
+            mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources(
                 {
                     {name = "buffer"}
