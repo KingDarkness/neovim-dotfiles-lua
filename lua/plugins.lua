58777a8f28abcd1857dfbb52997e33d90b7710b0
Nguyễn Trần Hoàn <nguyentranhoan13@gmail.com>
2022-11-30 16:30:26 +0700

    :boom:

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,6 +53,40 @@ function M.setup()
                 require("nvim-web-devicons").setup {default = true}
             end
         }
+        use(
+            {
+                "folke/noice.nvim",
+                requires = {"MunifTanjim/nui.nvim"},
+                config = function()
+                    require("noice").setup(
+                        {
+                            cmdline = {view = "cmdline"},
+                            notify = {enabled = false},
+                            messages = {enabled = false},
+                            health = {
+                                checker = false -- Disable if you don't want health checks to run
+                            },
+                            lsp = {
+                                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                                override = {
+                                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                                    ["vim.lsp.util.stylize_markdown"] = true,
+                                    ["cmp.entry.get_documentation"] = true
+                                }
+                            },
+                            -- you can enable a preset for easier configuration
+                            presets = {
+                                bottom_search = true, -- use a classic bottom cmdline for search
+                                command_palette = true, -- position the cmdline and popupmenu together
+                                long_message_to_split = true, -- long messages will be sent to a split
+                                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                                lsp_doc_border = false -- add a border to hover docs and signature help
+                            }
+                        }
+                    )
+                end
+            }
+        )
         -- highlighting
         use {
             "nvim-treesitter/nvim-treesitter",
@@ -379,13 +413,6 @@ function M.setup()
             end
         }
         -- search & replace
-        use {
-            "kevinhwang91/nvim-hlslens",
-            event = "VimEnter",
-            config = function()
-                require("hlslens").setup()
-            end
-        }
         use {
             "windwp/nvim-spectre",
             event = "VimEnter",
